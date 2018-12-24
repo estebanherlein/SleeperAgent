6a3c747fe47e87ab8a99820383281396a421eaab
estherlein <esherlein@gmail.com>
2018-12-23 21:13:59 -0300

    Removed unused import statement

--- instagram.py
+++ instagram.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import json
 import requests
 import objectpath
-import pprint
 
 _user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
